0ee76cf976e01fd60c545e54d2153483cd15da8f
jjadmire <hx405126485>
2019-10-25 19:12:53 +0800

    limit

--- src/main/java/com/hx199513/community/community/controller/IndexController.java
+++ src/main/java/com/hx199513/community/community/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.hx199513.community.community.controller;
 
+import com.hx199513.community.community.dto.PaginationDTO;
 import com.hx199513.community.community.dto.QuestionDTO;
 import com.hx199513.community.community.mapper.QuestionMapper;
 import com.hx199513.community.community.mapper.UserMapper;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -22,7 +24,10 @@ public class IndexController {
     @Autowired
     private QuestionService questionService;
     @GetMapping("/")
-    public String index(HttpServletRequest request, Model model){
+    public String index(HttpServletRequest request,
+                        Model model,
+                        @RequestParam(name="page",defaultValue = "1")Integer page,
+                        @RequestParam(name="size",defaultValue = "1")Integer size){
         Cookie[] cookies=request.getCookies();
         if(cookies!=null&&cookies.length!=0){
             for (Cookie cookie:cookies){
@@ -36,8 +41,8 @@ public class IndexController {
                 }
             }
         }
-        List<QuestionDTO> questionList=questionService.list();
-        model.addAttribute("questions",questionList);
+        PaginationDTO pagination=questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
     }
 }
